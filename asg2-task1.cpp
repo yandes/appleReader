ea8ac77ec7386342d8e2d6ad6dfc5abd3051f070
Foleosy <mywong@outlook.sg>
2016-10-12 04:37:32 +0800

    Incomplete support for function pointers

--- asg2-task1.cpp
+++ asg2-task1.cpp
@@ -14,67 +14,99 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-typedef std::unordered_set<std::string> setCalled;
-typedef std::unordered_set<std::string> setFunction;
-setCalled Scalls;
-setFunction Sfunc;
+	typedef std::unordered_set<std::string> setCalled;
+	typedef std::unordered_set<std::string> setFunction;
+	typedef std::unordered_set<std::string> setVariables;
+	setCalled Scalls;
+	setFunction Sfunc;
+	setVariables Svar;
 
-// Read the IR file.
-LLVMContext &Context = getGlobalContext();
-SMDiagnostic Err;
-Module *M = ParseIRFile(argv[1], Err, Context);
-if (M == NULL)
-{
-	fprintf(stderr, "error: failed to load LLVM IR file \"%s\"", argv[1]);
-	return EXIT_FAILURE;
-}
+	// Read the IR file.
+	LLVMContext &Context = getGlobalContext();
+	SMDiagnostic Err;
+	Module *M = ParseIRFile(argv[1], Err, Context);
+	if (M == NULL)
+	{
+		fprintf(stderr, "error: failed to load LLVM IR file \"%s\"", argv[1]);
+		return EXIT_FAILURE;
+	}
 
-for (auto curFref = M->getFunctionList().begin(), 
-              endFref = M->getFunctionList().end(); 
-              curFref != endFref; ++curFref) {
+	for (auto curFref = M->getFunctionList().begin(), 
+			endFref = M->getFunctionList().end(); 
+			curFref != endFref; ++curFref) {
 		string str = curFref->getName().str().c_str();
 		Sfunc.insert(str);
-        //printf("found function:%s\n",curFref->getName().str().c_str());
-}
 
 
-for (auto &F: *M){
-	for (auto &BB: F){
-		for (auto &I: BB){
-			CallInst *Call = dyn_cast<CallInst>(&I);
-			if (Call == NULL)
-			continue;
-			Function *G = Call->getCalledFunction();
-			if (G == NULL)
-			continue;
-			string str = G->getName().str().c_str();
-			Scalls.insert(str);
-			//printf("%s\n", G->getName().str().c_str());
-			//I.dump();
-		
+		//printf("found function:%s\n",curFref->getName().str().c_str());
+	}
+
+	// Function Pointers are stored as global variables
+	for (auto curVref = M->getGlobalList().begin(),
+			endVref = M->getGlobalList().end();
+			curVref != endVref; ++curVref) {
+		string var = curVref->getName().str().c_str();
+		Svar.insert(var);
+		//printf("Variable: %s\n", var);
+	}
+
+
+	for (auto &F: *M){
+		for (auto &BB: F){
+			for (auto &I: BB){
+
+				// S/O copied code that I have no idea how to use
+				if (StoreInst *si = dyn_cast<StoreInst>(&I)) {
+					Value* v = si->getPointerOperand();
+					Type* ptrType = v->getType()->getPointerElementType();
+					if (PointerType* pt = dyn_cast<PointerType>(ptrType)) {
+						do {
+							Type* pointedType = pt->getTypeAtIndex((unsigned int)0);
+							if (pointedType->isFunctionTy()) {
+								break;
+							}
+							ptrType = pointedType;
+						} while ((pt = (dyn_cast<PointerType>(ptrType))));
+					}
+					printf("Type: %s\n", ptrType);
+				}
+
+				CallInst *Call = dyn_cast<CallInst>(&I);
+				if (Call == NULL) {
+					continue;
+				}
+				Function *G = Call->getCalledFunction();
+				if (G == NULL) {
+					continue;
+				}
+				string str = G->getName().str().c_str();
+				Scalls.insert(str);
+				//printf("Called: %s\n", G->getName().str().c_str());
+				//I.dump();
+
 			}		
 		}
 	}
 
-printf("Total Number of Calls: %d\n",Scalls.size());
-printf("Total Number of Functions: %d\n",Sfunc.size());
+	printf("Total Number of Calls: %d\n",Scalls.size());
+	printf("Total Number of Functions: %d\n",Sfunc.size());
 
-int counter = 0;
-printf("List of Dead functions:\n");
-for(auto& elem: Sfunc){
-	std::unordered_set<std::string>::const_iterator got = Scalls.find (elem);
-	if ( got == Scalls.end() ){
-		if (elem != "main"){
-			std::cout <<elem <<endl;
-			counter++; 
+	int counter = 0;
+	printf("List of Dead functions:\n");
+	for(auto& elem: Sfunc){
+		std::unordered_set<std::string>::const_iterator got = Scalls.find (elem);
+		if ( got == Scalls.end() ){
+			if (elem != "main"){
+				std::cout <<elem <<endl;
+				counter++; 
+			}
 		}
 	}
-}
-printf("Total Number of Dead functions: %d\n",counter);
+	printf("Total Number of Dead functions: %d\n",counter);
 
 
 
 
 
-return 0;
+	return 0;
 }
